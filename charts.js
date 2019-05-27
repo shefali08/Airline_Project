6c686abf596fd1440610aacf2d535f72eb616db8
shefali08 <50987109+shefali08@users.noreply.github.com>
2019-05-27 18:23:56 +0530

    Added animation and code restructuring

--- charts.js
+++ charts.js
@@ -1,31 +1,67 @@
-//Plotting the chart
-let svgChart = document.getElementById('svgChart');
-
+/**
+ * calculates the svg coordinate data to be used to render the svg graph
+ * @param graphData - the processed graph data
+ * @param width - the available width for the graph
+ * @param height - the available height for the graph
+ * @returns {{coordinates}} - object containing array of the point coordinates
+ * and interval on x-axis
+ */
 function calculateSVGData(graphData, width, height) {
-    let values = graphData;
+    var values = graphData;
     return getCoordinates(values, width, height)
 }
 
+/**
+ * calculates the svg coordinate data for an array of values(an airline/all airlines)
+ * to be used to render the svg graph
+ * @param values - array of count of the airlines per hour
+ * @param width - the available width for the graph
+ * @param height - the available height for the graph
+ * @returns {{coordinates}} - object containing array of the point coordinates
+ * and interval on x-axis
+ */
 function getCoordinates(values, width, height) {
-    let min = Math.floor(Math.min.apply(null, values) * 0.95)
-    let max = Math.ceil(Math.max.apply(null, values) * 1.05)
+    var min = Math.floor(Math.min.apply(null, values) * 0.95)
+    var max = Math.ceil(Math.max.apply(null, values) * 1.05)
 
-    let yRatio = (max - min) / height
-    let xRatio = width / (values.length)
+    var yRatio = (max - min) / height
+    var xRatio = width / (values.length)
 
-    return values.map( (value, i) => {
-        let y = height - ((value - min) / yRatio);
-        let x = (xRatio * i) + (xRatio / 2);
-        let g = (xRatio * i);
+    return values.map(function (value, i) {
+        var y = !yRatio ? height : height - ((value - min) / yRatio);
+        var x = (xRatio * i) + (xRatio / 2);
+        var g = (xRatio * i);
         return [x, y, g]
     })
 }
 
-function plotSvgChart(svgData) {
+/**
+ * takes the generated svg coordinates and generates a command to draw svg path
+ * @param svgData
+ * @returns {string} lineData in svg path format
+ */
+function calculateLineData(svgData) {
     let lineData = "";
     svgData.map((coordinates, i) => {
-        let gldata = `m${coordinates[2]} 0 v200`
-        let gline = document.createElementNS(
+        if (i === 0) {
+            lineData = `m ${coordinates[0]},${coordinates[1]}`
+        }
+        else {
+            lineData = `${lineData} l ${coordinates[0] - svgData[i - 1][0]},${coordinates[1] - svgData[i - 1][1]}`
+        }
+    });
+    return lineData;
+}
+
+/**
+ * draws the vertical grid lines on the graph showing the 24 hours intervals
+ * @param svgElement SVG Chart
+ * @param svgData processed coordinates
+ */
+function drawGridLines(svgElement, svgData) {
+    svgData.map((coordinates, i) => {
+        var gldata = `m${coordinates[2]} 0 v200`
+        var gline = document.createElementNS(
             "http://www.w3.org/2000/svg",
             "path"
         );
@@ -33,22 +69,55 @@ function plotSvgChart(svgData) {
         gline.setAttribute("fill", "none");
         gline.setAttribute("stroke", "lightgray");
         gline.setAttribute("stroke-width", .5);
-        svgChart.appendChild(gline);
+        svgElement.appendChild(gline);
 
-        if (i === 0) {
-            lineData = `${lineData} m ${coordinates[0]},${coordinates[1]}`
-        }
-        else {
-            lineData = `${lineData} l ${coordinates[0] - svgData[i - 1][0]},${coordinates[1] - svgData[i - 1][1]}`
-        }
-    });
-    let line = document.createElementNS(
+    })
+}
+
+/**
+ * creates a path in the specified svg element based on the generated svg coordinates
+ * @param svgElement
+ * @param svgData
+ * @param prevSvgData
+ */
+function drawPath(svgElement, svgData, prevSvgData) {
+    var lineData = calculateLineData(svgData);
+    var prevLineData = calculateLineData(prevSvgData);
+    var line = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "path"
     );
-    line.setAttribute("d", lineData);
+    line.setAttribute("d", prevLineData);
     line.setAttribute("fill", "none");
     line.setAttribute("stroke", "#5CC0C0");
     line.setAttribute("stroke-width", 2);
-    svgChart.appendChild(line);
+    addAnimation(svgElement, line, "d", prevLineData, lineData);
+}
+
+/**
+ * attaches the animation to component of svg
+ * @param rootElement - the parent svg chart
+ * @param element - the element to be animated
+ * @param attributeName - the atribute of the element to be animated
+ * @param from - the start data of the animation
+ * @param to - the end data of the animation
+ */
+function addAnimation(rootElement, element, attributeName, from, to) {
+    var animate = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "animate"
+    );
+    animate.setAttribute("attributeName", attributeName);
+    animate.setAttribute("from", from);
+    animate.setAttribute("to", to);
+    animate.setAttribute("dur", '1s');
+    animate.setAttribute("begin", "indefinite");
+    animate.setAttribute("end", "indefinite");
+    animate.setAttribute("fill", "freeze");
+    animate.setAttribute("calcMode", "spline");
+    animate.setAttribute("keySplines", "0.5 0 0.5 1"); //ease-in-out
+    animate.setAttribute("keyTimes", "0;1");
+    element.appendChild(animate);
+    rootElement.appendChild(element);
+    animate.beginElement();
 }
--- main.js
+++ main.js
@@ -1,86 +1,139 @@
-this.airlines = airlines
-this.flights_jan_01_2008 = flights_jan_01_2008;
+const TOTAL_HOURS = 24;
+const ALL_AIRLINE = 'total';
+const ALL_AIRLINE_TEXT = 'All Airlines';
 let hour = [];
+let svgHeight = 0, svgWidth = 0;
+let svgData, prevSvgData, graphData;
+let totalFlightCount;
 
-//Finding valid flights from Flight Data
-let countColon = (time) => {
-  let count = 0;
-  for (let x of time) {
-    if (x === ':')
-      count++;
+/**
+ * Finds valid flights from available Flights Data
+ * @returns {*} - array of the flights having valid data
+ */
+let validateData = () => {
+  let countColon = (time) => {
+    let count = 0;
+    for (let x of time) {
+      if (x === ':')
+        count++;
+    }
+    if (count === 2)
+      return true;
   }
-  if (count === 2)
-    return true;
+  return flights_jan_01_2008.filter((item) => {
+    return (item && item.airline && item.time && countColon(item.time) && parseInt(item.time.substring(0, 2)) < TOTAL_HOURS)
+  });
 }
-let validFlights = flights_jan_01_2008.filter((item) => {
-  return (item && item.airline && item.time && countColon(item.time) && parseInt(item.time.substring(0, 2)) < 24)
-});
 
-const totalFlightCount = validFlights.length;
-// Creating the hour object
-// (() => {
-  for (let i = 0; i < 24; i++) {
+// Creates hour element which is an array of objects and populates it.
+// The hour has 24 objects for each hour and each object contains airlines
+// as keys and their count as values. 
+let parseData = (validFlights) => {
+  //Initializes an object with airline keys and a 'total' key to hold 
+  //count for respective ailrlines and total count per hour. 
+  for (let i = 0; i < TOTAL_HOURS; i++) {
     let obj = {};
-    obj.total = 0;
+    obj[ALL_AIRLINE] = 0;
     for (let key in airlines) {
       obj[key] = 0;
     }
     hour.push(obj);
   }
-// })();
 
-//segregation of data as per airline and hour
-validFlights.forEach( (item) => {
-  let i = parseInt(item.time.substring(0, 2));
-  let v = item.airline;
-  hour[i][v] += 1;
-  hour[i]['total'] += 1;
-})
-
-// graphData array for svg chart plotting
-let graphData = hour.map((i) => i.total)
-
-//Plotting the chart first time
-let svgViewBox = svgChart.getAttribute('viewBox');
-let split = svgViewBox.split(" ");
-let svgWidth = split[2];
-let svgHeight = split[3]
-let svgData = calculateSVGData(graphData, svgWidth, svgHeight);
-plotSvgChart(svgData);
-document.getElementById("totalFlightCount").innerHTML = `${totalFlightCount} Flights All Airlines`;
-
-
-//Filtering based on input text value
-let textInput = document.getElementById('searchText');
-let checkEnterKey = () => {
-  if (event.key === 'Enter')
-    filterAirlines();
+  //segregation of data as per airline and hour
+  validFlights.forEach((item) => {
+    let i = parseInt(item.time.substring(0, 2));
+    let v = item.airline;
+    hour[i][v] += 1;
+    hour[i][ALL_AIRLINE] += 1;
+  })
 }
 
-let filterAirlines = () => {
-  let flightAcronym = "";
-  let textValue = textInput.value.toUpperCase();
-  for (let key in airlines) {
-    let v = airlines[key].toUpperCase();
-    if (v.includes(textValue)) {
-      flightAcronym = key;
-      break;
-    }
-  }
+/**
+ * Returns flight array containing count for 24 hour
+ * @param attribute - the selected airline's key
+ * @returns {*} - array of the flight's count
+ */
+let getGraphData = (attribute) => {
+  return hour.map((i) => i[attribute] || 0)
+}
 
-  let newGraphData = hour.map((i) => i[flightAcronym]);
-  let flightCount = hour.reduce((total, val) => { return total + val[flightAcronym] }, 0);
-  while (svgChart.firstChild) {
-    svgChart.removeChild(svgChart.firstChild);
-  }
-  if (textValue === "" || !flightAcronym) {
-    svgData = calculateSVGData(graphData, svgWidth, svgHeight);
-    plotSvgChart(svgData);
-    document.getElementById("totalFlightCount").innerHTML = `${totalFlightCount} Flights All Airlines`;
-  }
-  else {
-    svgData = calculateSVGData(newGraphData, svgWidth, svgHeight);
-    plotSvgChart(svgData);
-    document.getElementById("totalFlightCount").innerHTML = `${flightCount} Flights ${airlines[flightAcronym]}`;
+/**
+ * Returns total number of flights
+ * @param attribute - the selected airline's key
+ * @returns {*} - Total value
+ */
+let getflightCount = (attribute) => {
+  return hour.reduce((total, val) => { return total + val[attribute] }, 0);
+}
+
+//Initializes the graph parameters
+let initializeGraphParameters = () => {
+  var svgChart = document.getElementById('svgChart');
+  let svgViewBox = svgChart.getAttribute('viewBox');
+  let split = svgViewBox.split(" ");
+  svgWidth = split[2];
+  svgHeight = split[3];
+}
+
+/**
+ * Calculates coordinates for the points on line chart and plots the chart 
+ * @param graphData - the processed graphData to be plotted
+ * @param count - count of selected airline
+ * @param displayName - Name of the selected airline
+ */
+let drawGraph = (graphData, count, displayName) => {
+  svgData = calculateSVGData(graphData, svgWidth, svgHeight);
+  drawGridLines(svgChart, svgData);
+  drawPath(svgChart, svgData, prevSvgData);
+  prevSvgData = svgData;
+  document.getElementById("totalFlightCount").innerHTML = `${count} Flights ${displayName}`;
+}
+
+//When page is loaded, validates and parses the data,and renders graph
+let init = () => {
+  validFlights = validateData();
+  parseData(validFlights);
+  initializeGraphParameters();
+  prevSvgData = calculateSVGData(getGraphData(), svgWidth, svgHeight);
+  totalFlightCount = getflightCount(ALL_AIRLINE);
+  graphData = getGraphData(ALL_AIRLINE);
+  drawGraph(graphData, totalFlightCount, ALL_AIRLINE_TEXT);
+}
+//Function to run when the page is loaded
+window.onload = init();
+
+/**
+ * Re-renders graph based on input text value
+ * @param e
+ */
+let filterAirlines = (e) => {
+  let textInput = document.getElementById('searchText');
+  if (e.type === 'keypress' && e.key === 'Enter' || e.type === 'focusout') {
+    let flightAcronym = "";
+    let textValue = textInput.value.toUpperCase();
+    for (let key in airlines) {
+      let v = airlines[key].toUpperCase();
+      if (v.includes(textValue)) {
+        flightAcronym = key;
+        break;
+      }
+    }
+
+    let newGraphData = getGraphData(flightAcronym);
+    let flightCount = getflightCount(flightAcronym);
+    while (svgChart.firstChild) {
+      svgChart.removeChild(svgChart.firstChild);
+    }
+    if (!flightAcronym && textValue !== "") {
+      alert("Airline does not exist");
+      e.target.value = "";
+    }
+    else if (textValue === "") {
+      drawGraph(graphData, totalFlightCount, ALL_AIRLINE_TEXT);
+    }
+    else {
+      drawGraph(newGraphData, flightCount, airlines[flightAcronym]);
+    }
   }
 };
